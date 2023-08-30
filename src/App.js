9ff841af4c05f83cfb49bd789b0f15bc48831d8b
majumxrs <mariaescola06@gmail.com>
2023-08-30 09:25:18 -0300

    TUDO FUNCIONANDO

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 function App() {
 
   const [listaTarefas, setListaTarefas] = useState( [] );
-  const [tarefa, setTarefas] = useState( { id: '' , texto: "" } ); 
+  const [tarefa, setTarefas] = useState( { id: '' , texto: "" , status: "" } ); 
 
   function AddTarefa()
   {
@@ -16,11 +16,18 @@ function App() {
     const novaLista = listaTarefas.filter( ( tarefa ) => tarefa.id !== id );
     setListaTarefas( novaLista );
   }
+  function statusTarefa( id, status )
+  {
+      const index = listaTarefas. findIndex( (tarefa) => tarefa.id === id );
+      const novoStatus = status;
+      listaTarefas[index].status = !status;
+      setListaTarefas( [...listaTarefas ] );
+  }
   
     
 
   useEffect( () => {
-    setTarefas( { id: '' , texto: "" } );
+    setTarefas( { id: '' , texto: "", status:"" } );
   }, [ listaTarefas ] )
 
   return (
@@ -29,14 +36,14 @@ function App() {
         <h1>React DO</h1>
       </header>
       <div>
-        <input type="text" name="tarefa" placeholder="Insira aqui sua tarefa" value={tarefa.texto} onChange={ (e) => setTarefas( { id: Math.random(), texto: e.target.value } )}/>
+        <input type="text" name="tarefa" placeholder="Insira aqui sua tarefa" value={tarefa.texto} onChange={ (e) => setTarefas( { id: Math.random(), texto: e.target.value, status: false} )}/>
         <button onClick={AddTarefa}>ADD</button>
         
       </div>
       <div>
         <ul>
         {listaTarefas.map( (item, index ) => (
-          <li key={index}>{item.texto} <button onClick={ () => excluirTarefa(item.id)}>Excluir</button></li>
+          <li key={index}>{item.texto}   <button onClick={ () => statusTarefa(item.id, item.status)}>{item.status ? "Concluida" : "Não concluida"}</button>  <button onClick={ () => excluirTarefa(item.id)}>Excluir</button></li>
         ))}
         </ul>
       </div>
